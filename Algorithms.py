0ad043c68cf2bc5943dea1caa2bad0795b590246
Noah <noahenicholls@gmail.com>
2022-03-23 17:29:54 -0400

    Add files via upload

---
+++ Algorithms.py
@@ -0,0 +1,31 @@
+#Function: lowercase words
+def lowercase(lower_list):
+
+#Function: lowercase words with special characters (SC) 
+def lowercase_SC(lower_list, special_characters):
+
+#Function: lowercase words with numbers 0-9999
+def lowercase_numbers(lower_list):
+
+#Function: lowercase words with special numbers list
+def lowercase_SN(lower_words, special_numbers):
+
+#Function: lowercase words with special numbers and special characters
+def lowercase_SN_SC(lower_SN_SC_list, special_numbers, special_characters):
+
+#Function: Uppercase words
+def uppercase(upper_list):
+
+#Function: Uppercase words with special characters (SC) 
+def uppercase_SC(upper_list, special_characters):
+
+#Function: Uppercase words with numbers 0-9999
+def uppercase_numbers(upper_list):
+
+#Function: Uppercase words with special numbers and special characters
+def uppercase_SN(upper_list, special_numbers):
+
+#Function: Uppercase words with special numbers and special characters
+def uppercase_SN_SC(upper_list, special_numbers, special_characters):
+
+
---
+++ MISI 788 Application Nicholls 1_0.py
@@ -0,0 +1,56 @@
+import os
+from csv import reader
+import time
+
+"""
+Noah Earl Nicholls
+MISI 788 Specialty Project
+APPLYING HUMAN INTELLIGENCE TO OFFENSIVE CYBER OPERATIONS AND DIGITAL FORENSICS: A PYTHON FRAMEWORK
+Version 1.0
+March 22, 2022
+"""
+
+inputFile = r'D:\MISI788\PROGRAM\HUMINT_TEMPLATE.csv'
+wordlist = r'D:\MISI788\PROGRAM\PasswordList.txt'
+
+def lowercase(lower_list):
+    for item in lower_list:
+        string = item.lower()
+        print("writing: ", string)
+        f.write(string)
+        f.write('\n')
+
+#Open CSV file, parse 2nd row into list
+x=0
+with open (inputFile) as csv_file:
+    csv_reader = reader(csv_file, delimiter = '\t')
+    for row in csv_reader:
+        if (x == 1):
+            password_factors = list(row)
+        x+=1
+
+#Print list for user
+print ("The HUMINT factors that will be used to generate passwords are as follows \n", password_factors)
+
+#Generate lists
+special_numbers = []
+special_words = []
+special_characters = ['!', '@', '#', '$', '%']
+
+#Separate password_factors into two lists: special_numbers, special_words
+for x in password_factors:
+    try:
+        word = int(x)
+        special_numbers.append(word)
+    except:
+        try:
+            number = str(x)
+            special_words.append(number)
+        except:
+            print("Template error, ensure entries are only in second row.")
+
+#Test Print
+#print (special_numbers, special_words)
+            
+with open(wordlist, 'w') as f:
+    lowercase(special_words)
