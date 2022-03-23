2291f887ddf7b2c0012b0119d2388fcf25dbf4c5
Noah <noahenicholls@gmail.com>
2022-03-23 18:01:21 -0400

    Rename MISI 788 Application to MAIN

---
+++ Handler.py
@@ -0,0 +1,7 @@
+from Algorithms import *
+from main import *
+
+with open(wordlist, 'w') as f:
+    def generate(special_words, special_numbers, special_characters):
+        lowercase(special_words);
+        uppercase(special_words);
---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+from csv import reader
+import time
+from Handler import *
+
+"""
+Noah Earl Nicholls
+MISI 788 Specialty Project
+APPLYING HUMAN INTELLIGENCE TO OFFENSIVE CYBER OPERATIONS AND DIGITAL FORENSICS: A PYTHON FRAMEWORK
+Version 1.1
+March 22, 2022
+"""
+
+inputFile = r'D:\MISI788\PROGRAM\HUMINT_TEMPLATE.csv'
+wordlist = r'D:\MISI788\PROGRAM\PasswordList.txt'
+
+#Generate lists
+special_numbers = []
+special_words = []
+special_characters = ['!', '@', '#', '$', '%']
+
+#Open CSV file, parse 2nd row into list
+x=0
+with open (inputFile) as csv_file:
+    csv_reader = reader(csv_file, delimiter = '\t')
+    for row in csv_reader:
+        if (x == 1):
+            password_factors = list(row)
+        x+=1
+
+#Print list for user
+print ("The HUMINT factors that will be used to generate passwords are as follows: \n", password_factors)
+
+#Separate password_factors into two lists: special_numbers, special_words
+for x in password_factors:
+    try:
+        word = int(x)
+        special_numbers.append(word)
+    except:
+        try:
+            number = str(x)
+            special_words.append(number)
+        except:
+            print("Template error, ensure entries are only in second row.")
+
+#Test Print
+#print (special_numbers, special_words)
+            
+
+Handler(special_words, special_numbers, special_characters)
+print("Wordlist generated.")
